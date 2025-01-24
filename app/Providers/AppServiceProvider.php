fbfb6d442aed07658e18ca2c9517b8208ceac9e8
reza-fauzan <142280735+reza-fauzan@users.noreply.github.com>
2025-01-25 06:34:38 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config(key: 'app.env') === 'production') {
+            URL::forceScheme(scheme: 'https');
+        }
     }
 }
